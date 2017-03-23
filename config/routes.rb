72cd9012677c11b429a298c2dbf36ae5402d59c5
beach17 <aalajor@yahoo.com>
2017-03-23 19:36:32 +0800

    Login and Logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :organizations
  
 	get 'home' => "organizations#home"
 	get 'about' => "organizations#about"
 	get 'login' => "organizations#login"
+
+	root :to => "organizations#home"
 	
 end
