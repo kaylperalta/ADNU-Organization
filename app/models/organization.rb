2ac2835f6a16809c38fbdb67cdb8b44c165ea86f
beach17 <aalajor@yahoo.com>
2017-03-23 16:15:52 +0800

    Arvin Alajor

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,2 +1,16 @@
 class Organization < ActiveRecord::Base
+validates :name, :presence => true,
+				:length => { :minimum => 3 }
+				
+validates :classification, :length => {:minimum => 3}	
+
+validates :org_identification, :numericality => {:only_integer => true}	
+
+validates :username, uniqueness: true,
+		  :length => {:within => 8..25,}
+validates :password, confirmation: true, :length => { :within => 7..28,}
+
+
+
 end
+
