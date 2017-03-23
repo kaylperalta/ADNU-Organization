b3acc40f8b4f2569056fa90e1a21b426c93b1d16
rombitalanis <jose.alanis06@gmail.com>
2017-03-23 19:22:47 +0800

    web interface

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,16 +1,2 @@
 class Organization < ActiveRecord::Base
-validates :name, :presence => true,
-				:length => { :minimum => 3 }
-				
-validates :classification, :length => {:minimum => 3}	
-
-validates :org_identification, :numericality => {:only_integer => true}	
-
-validates :username, uniqueness: true,
-		  :length => {:within => 8..25,}
-validates :password, confirmation: true, :length => { :within => 7..28,}
-
-
-
 end
-
